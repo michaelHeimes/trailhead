de48c5bf3f5fc80f513f893650eefcb852ff3cfa
Michael Heimes <michael@cairndigitalmedia.com>
2022-10-04 14:53:34 -0400

    Removed more unneeded files

--- template-parts/loop-modules.php
+++ template-parts/loop-modules.php
@@ -8,13 +8,13 @@
 		if( have_rows('page_modules') ):
 			while ( have_rows('page_modules') ) : the_row();
 
-				if( get_row_layout() == 'graphical_callout' ):
-					get_template_part('template-parts/modules/graphical-callout');
-				elseif( get_row_layout() == 'image_&_copy_card' ):
-					get_template_part('template-parts/modules/part-image-copy-card');
-				elseif( get_row_layout() == 'text' ):
-					get_template_part('template-parts/modules/text');
-				endif;
+				// if( get_row_layout() == 'graphical_callout' ):
+				// 	get_template_part('template-parts/modules/graphical-callout');
+				// elseif( get_row_layout() == 'image_&_copy_card' ):
+				// 	get_template_part('template-parts/modules/part-image-copy-card');
+				// elseif( get_row_layout() == 'text' ):
+				// 	get_template_part('template-parts/modules/text');
+				// endif;
 	
 			endwhile;
 		endif;
