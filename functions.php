0ffd943b2ee810874d604c228430608aca22c481
Michael Heimes <michael@cairndigitalmedia.com>
2023-11-19 13:30:37 -0500

    Google font function

--- functions.php
+++ functions.php
@@ -170,6 +170,20 @@ function trailhead_scripts() {
 add_action( 'wp_enqueue_scripts', 'trailhead_scripts' );
 
 
+/**
+ * Enqueue Google Fonts.
+ */
+function enqueue_google_fonts() {
+	// Enqueue the Google Fonts stylesheet
+	wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Tenor+Sans&display=swapp', array(), null);
+
+	// Preload the Google Fonts stylesheet
+	add_action('wp_head', function () {
+		echo '<link rel="preload" href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Tenor+Sans&display=swap" as="style" onload="this.onload=null;this.rel=\'stylesheet\'">';
+	});
+}
+
+
 // Disable Tabelpress Stylesheet
 add_filter( 'tablepress_use_default_css', '__return_false' );
 
--- header.php
+++ header.php
@@ -16,10 +16,6 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Jockey+One&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 
 	<?php wp_head(); ?>
 </head>
