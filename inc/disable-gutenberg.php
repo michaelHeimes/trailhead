1e976e15c2a378ac1b300445754890e9d549fb7d
Michael Heimes <michael@cairndigitalmedia.com>
2024-04-22 09:32:43 -0400

    gutenberg stye removal

--- inc/disable-gutenberg.php
+++ inc/disable-gutenberg.php
@@ -3,6 +3,14 @@
 // Disable Gutenberg
 add_filter( 'use_block_editor_for_post', '__return_false' );
 
+//Remove Gutenberg Block Library CSS from loading on the frontend
+function smartwp_remove_wp_block_library_css(){
+ wp_dequeue_style( 'wp-block-library' );
+ wp_dequeue_style( 'wp-block-library-theme' );
+ wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
+} 
+add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
+
 // Disable Gutenberg Post Type
 /*
 add_filter('use_block_editor_for_post_type', 'prefix_disable_gutenberg', 10, 2);
